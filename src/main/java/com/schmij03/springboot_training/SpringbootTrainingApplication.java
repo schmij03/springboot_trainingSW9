de58f90d4a93f83165f4a30ebe14780d42485e80
schmij03 <96172389+schmij03@users.noreply.github.com>
2023-05-23 17:24:31 +0200

    first commit

---
+++ src/main/java/com/schmij03/springboot_training/SpringbootTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.schmij03.springboot_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schmij03/springboot_training/SpringbootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schmij03.springboot_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
